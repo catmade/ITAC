521a673debbd70b48bffdc0c5f05ccb098d712ec
AgentAnderson <neotheone233@gmail.com>
2019-04-19 19:31:12 +0800

    删除多余分号

--- src/coding/shannon/Shannon.java
+++ src/coding/shannon/Shannon.java
@@ -1,6 +1,20 @@
 package coding.shannon;
-
+/*
+    i   p(ai)   pa(aj)  ki  码字
+    1   0.25    0.000   2   00
+    2   0.25    0.250   2   01
+    3   0.20    0.500   3   100
+    4   0.15    0.700   3   101
+    5   0.10    0.850   4   1101
+    6   0.05    0.950   5   111110
+ */
 public class Shannon {
+    private int i;//    信源符号编号
+    private int ki;//   码长
+
+
+
+
     public Shannon(){
         System.out.println("HelloWorld");
     }
